baf52b9776ed4da25f89745b8416aaad8ea3c5b6
Kojobu <tom.schlenker@gmx.de>
2024-07-11 16:25:16 +0200

    mp now working

--- sbc.py
+++ sbc.py
@@ -5,88 +5,30 @@ from dataloader import *
 my_module_path = os.path.join("./", "..", '21cm-wrapper')
 sys.path.append(my_module_path)
 from Leaf import *
-
-# hyperparams
-data_path = "./data/"
-batch_size = 8
-epochs = 120
-train_test_data_ration = 0.95
-
-optimizer = torch.optim.Adam
-optimizer_params = {
-    "lr": 6e-4,
-}
-
-loss = torch.nn.MSELoss
-loss_params = {}
-
-norm_range = torch.tensor([
-            [0.3,10.0], # M_WDM
-            [0.2,0.4], # OMm
-            [38, 42], # L_X
-            [100, 1500], # NU_X_THRESH
-            [4, 5.3], # ION_Tvir_MIN
-            [10.0, 250.0], # HII_EFF_FACTOR
-], dtype = torch.float32)
-
-
-# transform trainingsdata
-# perhaps add check if file is there: continue + override option in the future
-#convert_to_torch(path = data_path, prefix="run", redshift_cutoff=600, debug=False, statistics=True)
-
-# load data
-train_data = DataHandler(path=data_path, prefix="batch", load_to_ram=False,
-                            split = train_test_data_ration, training_data = True,
-                            apply_norm=True, norm_range=norm_range, augmentation_probability=0)
-test_data = DataHandler(path=data_path, prefix="batch", load_to_ram=False,
-                            split = train_test_data_ration, training_data = False,
-                            apply_norm=True, norm_range=norm_range, augmentation_probability=0)
-# import data to torch dataloader
-train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True,
-                                num_workers = 2, pin_memory = True, prefetch_factor=2)
-test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False,
-                                num_workers = 2, pin_memory = True, prefetch_factor=2)
-
-# init model
-model = ModelHandler(Model = Summary_net_lc_smol,
-                        Training_data=train_dataloader, Test_data=test_dataloader, device='cpu')
-
-from sbi.inference import SNPE, SNLE
-from sbi import utils, analysis
-
-# define model hyperparemeter
-user_params = {
-"HII_DIM": 40,
-"BOX_LEN": 160,
-"N_THREADS": 1,
-"USE_INTERPOLATION_TABLES": True,
-"PERTURB_ON_HIGH_RES": True
-}
-
-flag_options = {
-"INHOMO_RECO": True,
-"USE_TS_FLUCT": True
-}
-
-#simparams = p21c.outputs.LightCone.read("./data/run_36690")
-# load the simulator class
-Leaf_simulator = Leaf(debug=True, user_params=user_params, flag_options=flag_options, redshift = 5.5)
-
-# define the prior ranges (only need for denormalization!)
-prior_range = torch.tensor([
-            [0.3,10.0], # M_WDM
-            [0.2,0.4], # OMm
-            [38, 42], # L_X
-            [100, 1500], # NU_X_THRESH
-            [4, 5.3], # ION_Tvir_MIN
-            [10.0, 250.0], # HII_EFF_FACTOR
-], dtype = torch.float32)
-
-# define the prior (uniform prior in this case)
-prior = utils.BoxUniform(low=torch.zeros((6)), high=torch.ones((6)))
+from sbi.utils.get_nn_models import (
+        posterior_nn,
+    )  # For SNPE: posterior_nn(), SNLE: likelihood_nn(). For SNRE: classifier_nn()
 
 # define the simulator
 def simulation(theta):
+
+    user_params = {
+    "HII_DIM": 40,
+    "BOX_LEN": 160,
+    "N_THREADS": 1,
+    "USE_INTERPOLATION_TABLES": True,
+    "PERTURB_ON_HIGH_RES": True
+    }
+
+    flag_options = {
+    "INHOMO_RECO": True,
+    "USE_TS_FLUCT": True
+    }
+
+    #simparams = p21c.outputs.LightCone.read("./data/run_36690")
+    # load the simulator class
+    Leaf_simulator = Leaf(debug=False, user_params=user_params, flag_options=flag_options, redshift = 5.5)
+
     M_WDM, OMm, L_X, NU_X_THRESH, ION_Tvir_MIN, HII_EFF_FACTOR = theta
     cosmo_params = {
         "OMm": OMm
@@ -96,41 +38,107 @@ def simulation(theta):
         "NU_X_THRESH": NU_X_THRESH,
         "ION_Tvir_MIN": ION_Tvir_MIN,
         "HII_EFF_FACTOR": HII_EFF_FACTOR,
-        "INHOMO_RECO": True
     }
     global_params = {
         "M_WDM": M_WDM
     }
 
-    res = torch.as_tensor(Leaf_simulator.run_lightcone(
+    res = Leaf_simulator.run_lightcone(
         save = False, sanity_check = True, filter_peculiar = False,
-        astro_params = astro_params, global_params = global_params, cosmo_params = cosmo_params).brightness_temp, dtype=torch.float32)
-    
-    return (res, torch.as_tensor(theta, dtype=torch.float32))
+        astro_params = astro_params, global_params = global_params, cosmo_params = cosmo_params).brightness_temp[:,:,:600]
+    print('done simulating')
+    return res, theta
 
 
 def simulator(theta: torch.FloatTensor, Model: object, data_loader: object, threads: int = 1):
     tshape = theta.shape
     schwimmhalle = Pool(max_workers=threads, max_tasks_per_child=1, mp_context=get_context('spawn'))
-    #runner = [params.tolist() for params in theta]
-    result = torch.empty((0,tshape[1]), dtype=torch.float32)
-    with alive_bar(tshape[0], force_tty=True) as bar: 
+    paramater = torch.empty((0, tshape[1]), dtype=torch.float32)
+    first = True
+    with alive_bar(tshape[0], force_tty=True, refresh_secs=5) as bar: 
         with schwimmhalle as p:
-            futures = {executor.submit(simulation, params.tolist()): params.tolist() for params in theta}
+            futures = [p.submit(simulation, params.tolist()) for params in theta]
             for future in as_completed(futures):
                 lc_bt, lab = future.result()
-                bt, lab = data_loader.normalize(images=torch.unsqueeze(bt, dim=0), labels=torch.unsqueeze(lab, dim=0))
-                pred = model.fast_forward(bt)
-                result = torch.cat((result, pred),0)    
+                lc_bt, lab = torch.as_tensor(lc_bt, dtype=torch.float32), torch.as_tensor(lab, dtype=torch.float32)
+                bt, lab = data_loader.normalize(images=torch.unsqueeze(lc_bt, dim=0), labels=torch.unsqueeze(lab, dim=0))
+                pred = Model.fast_forward(bt)
+                if first:
+                    x = torch.empty(pred.shape, dtype=torch.float32)
+                # may be unsorted
+                x = torch.cat((x, pred),0)    
+                parameter = torch.cat((paramater, lab),0)
                 bar()
-    return result
+    return x, parameter
+
+
 
 if __name__ == '__main__':
-    ### SNPE ###
+    # hyperparams
+    data_path = "./data/"
+    batch_size = 8
+    epochs = 120
+    train_test_data_ration = 0.95
 
-    from sbi.utils.get_nn_models import (
-        posterior_nn,
-    )  # For SNPE: posterior_nn(), SNLE: likelihood_nn(). For SNRE: classifier_nn()
+    optimizer = torch.optim.Adam
+    optimizer_params = {
+        "lr": 6e-4,
+    }
+
+    loss = torch.nn.MSELoss
+    loss_params = {}
+
+    norm_range = torch.tensor([
+                [0.3,10.0], # M_WDM
+                [0.2,0.4], # OMm
+                [38, 42], # L_X
+                [100, 1500], # NU_X_THRESH
+                [4, 5.3], # ION_Tvir_MIN
+                [10.0, 250.0], # HII_EFF_FACTOR
+    ], dtype = torch.float32)
+
+
+    # transform trainingsdata
+    # perhaps add check if file is there: continue + override option in the future
+    #convert_to_torch(path = data_path, prefix="run", redshift_cutoff=600, debug=False, statistics=True)
+
+    # load data
+    train_data = DataHandler(path=data_path, prefix="batch", load_to_ram=False,
+                                split = train_test_data_ration, training_data = True,
+                                apply_norm=True, norm_range=norm_range, augmentation_probability=0)
+    test_data = DataHandler(path=data_path, prefix="batch", load_to_ram=False,
+                                split = train_test_data_ration, training_data = False,
+                                apply_norm=True, norm_range=norm_range, augmentation_probability=0)
+    # import data to torch dataloader
+    train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True,
+                                    num_workers = 2, pin_memory = True, prefetch_factor=2)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False,
+                                    num_workers = 2, pin_memory = True, prefetch_factor=2)
+
+    # init model
+    model = ModelHandler(Model = Summary_net_lc_smol,
+                            Training_data=train_dataloader, Test_data=test_dataloader, device='cpu')
+
+    from sbi.inference import SNPE, SNLE
+    from sbi import utils, analysis
+
+    # define model hyperparemeter
+
+
+    # define the prior ranges (only need for denormalization!)
+    prior_range = torch.tensor([
+                [0.3,10.0], # M_WDM
+                [0.2,0.4], # OMm
+                [38, 42], # L_X
+                [100, 1500], # NU_X_THRESH
+                [4, 5.3], # ION_Tvir_MIN
+                [10.0, 250.0], # HII_EFF_FACTOR
+    ], dtype = torch.float32)
+
+    # define the prior (uniform prior in this case)
+    prior = utils.BoxUniform(low=torch.zeros((6)), high=torch.ones((6)))
+
+    ### SNPE ###
 
     # load the summary model
     model = ModelHandler(Model = Summary_net_lc_smol, device='cpu')
@@ -158,11 +166,11 @@ if __name__ == '__main__':
 
     # We draw theta samples from the posterior. This part is not in the scope of SBI
 
-    posterior_samples = train_data.denormalize(labels=posterior.sample((5000,)))
+    posterior_samples = train_data.denormalize(labels=posterior.sample((50,)))
 
     # We use posterior theta samples to generate x data
 
-    x_pp = torch.as_tensor(simulator(theta = posterior_samples, Model = model, threads=6, data_loader=train_data))
+    x_pp, x_o = torch.as_tensor(simulator(theta = posterior_samples, Model = model, threads=6, data_loader=train_data))
 
     # We verify if the observed data falls within the support of the generated data
     fig, _ = analysis.pairplot(
@@ -171,7 +179,8 @@ if __name__ == '__main__':
         limits=[[0, 1],[0, 1],[0, 1],[0, 1],[0, 1],[0, 1],], figsize=(5, 5),
         labels = ["M_WDM", "OMm", "L_X", "NU_X_THRESH", "ION_Tvir_MIN", "HII_EFF_FACTOR"],
     )
-    
+    fig.savefig("sbc.png", dpi=300)
+
 
 
 
