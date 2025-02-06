fix normalization, it should now work as intended

cl_models.py
@@ -20,7 +20,7 @@ DEFAULT_MIN_DERIVATIVE = 1e-3
 class RNVP(nn.Module):
     def __init__(self, in_dim: int, n_blocks: int, n_nodes: int, cond_dim: int = 6,
                  hidden_layer = 1, batch_norm: bool = False, 
-                 activation = 'relu', device = 'cuda', epsilon = 1e-4):
+                 activation = 'relu', device = 'cuda', epsilon = 1e-3):
         super().__init__()
         self.in_dim = in_dim
         self.n_blocks = n_blocks

classifier.py
@@ -61,7 +61,7 @@ class BasicBlock(nn.Module):
 
 class ResNet(nn.Module):
     def __init__(self, in_dim, n_nodes, hidden_layer, batch_norm, n_blocks, K=4, gamma=1.0, activation_fn = nn.ReLU(),
-                 device = 'cuda', prior = BoxUniform):
+                 device = 'cuda', prior = BoxUniform, epsilon: float = 1e-3):
         super(ResNet, self).__init__()
         self.in_dim = in_dim
         self.classifier = []